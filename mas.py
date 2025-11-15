000907cc06d1ed0d3975ecd79e39447edee99384
sinanazakli <sinanazakli@users.noreply.github.com>
2025-11-15 18:01:18 +0300

    modify mas.py

--- mas.py
+++ mas.py
@@ -6,30 +6,32 @@ import pandas as pd
 
 format_model_list(AutoMLClassifierProPlus.model_list)
 
-# # Datensatz laden
+# load dataset example 
+# load here whatever dataset you want to use
 iris = load_iris()
 
-# # AutoML-Instanzen erstellen
+# create automl object to use the AutoML functionality
 automl_plus = AutoMLClassifierProPlus()
 
-# # test_data = pd.read_csv("test.csv")
-# # test_data = test_data.sample(n=5000, random_state=42)  # z. B. 5000 Zeilen
-# # test_data = test_data.sample(frac=0.01, random_state=42)  # 10 % des Datensatzes
+# if you have big data for example you can sample it down like this
+# test_data = pd.read_csv("test.csv")
+# test_data = test_data.sample(n=5000, random_state=42)  # e.g. 5000 rows
+# test_data = test_data.sample(frac=0.01, random_state=42)  # e.g. 10 % of the data
 
-# # automl mit Standardwerten
-# # df_results, best_model = automl.find(iris)
+# automl mit Standardwerten
+# df_results, best_model = automl.find(iris)
 # df_results, best_model = automl_plus.find(iris, low_mem=True, reduce_to=0.1)
 df_results, best_model = automl_plus.find(iris, low_mem=True, reduce=10)
 
-# # Eigene Parameter
-# # df_results, best_model = automl.find(iris, test_size=0.3, random_state=123)
+# use test_size and random_state for your own needs
+# df_results, best_model = automl.find(iris, test_size=0.3, random_state=123)
 
-# Ergebnisse anzeigen
-print("\nErgebnisse:")
+# print results
+print("\nResults:")
 print(df_results)
-print("\nBestes Modell:")
+print("\nBest Modell:")
 print(best_model)
 
-print("\nBestes Modell mit formatierter Ausgabe:")
-
+# print best model with formatted output
+print("\nBest Model with formatet output:")
 print(format_pipeline_info(best_model))
